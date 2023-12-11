f284f187748153caa286a7c41b2a79918e2c8729
Agubaeze-Peace <agubaezep@gmail.com>
2023-12-11 13:21:29 -0800

    Added menu icon

---
+++ index.js
@@ -0,0 +1,6 @@
+const menuopen =document.querySelector("#mobile-menu-open")
+const menuclose = document.querySelector("mobile-menu-close#")
+const menu = document.querySelector("#menu")
+
+menuopen.addEventListener
+
